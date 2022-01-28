797c66ae050664227481f1404a6015a7b0e8a8a2
sandhya <sandhya.lavuri07@gmail.com>
2022-01-28 21:18:14 +0530

    changed url

--- src/api/Mc.js
+++ src/api/Mc.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const urlMerchant = "http://localhost:5000/merchant";
+const urlMerchant = "https://multi-merchant-server.herokuapp.com/merchant";
 export const fetchMerchant = () => axios.get(urlMerchant)
 export const createMerchant = (newMerchant)=> axios.post(urlMerchant,newMerchant);
 export const updateMerchant =(id,updateMerchant)=>axios.patch(`${urlMerchant}/${id}`,updateMerchant);
 export const deleteMerchant = (id)=> axios.delete(`${urlMerchant}/${id}`);
 
-const urlCustomer = "http://localhost:5000/customer";
+const urlCustomer = "https://multi-merchant-server.herokuapp.com/customer";
 export const fetchCustomer = () => axios.get(urlCustomer)
 export const createCustomer = (newCustomer)=> axios.post(urlCustomer,newCustomer);
 export const updateCustomer =(id,updateCustomer)=>axios.patch(`${urlCustomer}/${id}`,updateCustomer);
