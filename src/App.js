767c324e4da616ca20bbefe888ba6a83b6f9bc10
sandhya <sandhya.lavuri07@gmail.com>
2022-01-28 20:57:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Home from "./Components/Home/Home";
+import MerchantForm from "./Components/Merchants/MerchantForm";
+import CustomerForm from "./Components/Customer/CustomerForm";
+import MerchantsData from "./Components/Merchants/MerchantsData";
+import CustomerData from "./Components/Customer/CustomerData";
+import UpdateM from "./Components/Merchants/UpdateMerchant/UpdateM"
+import UpdateC from "./Components/Customer/UpdateCustomer/UpdateC"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Router>
+        <div>
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/MerchantForm" exact element={<MerchantForm />} />
+            <Route path="/CustomerForm" exact element={<CustomerForm />} />
+            <Route path="/MerchantData" exact element = {<MerchantsData/>}/>
+            <Route path="/CustomerData" exact element= {<CustomerData/>}/>
+            <Route path ="/UpdateM" exact element = {<UpdateM/>}/>
+            <Route path = "/UpdateC" exact element = {<UpdateC/>}/>
+          </Routes>
+        </div>
+      </Router>
+  
+   
+   </>
   );
 }
 
