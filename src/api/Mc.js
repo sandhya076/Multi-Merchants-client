bb3db0c358507333262b109346b92521e0ba6554
sandhya076 <lavurisandhya76@gamil.com>
2022-08-12 21:23:45 +0530

    first commit

--- src/api/Mc.js
+++ src/api/Mc.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const urlMerchant = "http://localhost:5000/merchant";
+const urlMerchant = "https://multi-server1.herokuapp.com/merchant";
 export const fetchMerchant = () => axios.get(urlMerchant);
 export const createMerchant = (newMerchant) =>
   axios.post(urlMerchant, newMerchant);
@@ -8,7 +8,7 @@ export const updateMerchant = (id, updateMerchant) =>
   axios.patch(`${urlMerchant}/${id}`, updateMerchant);
 export const deleteMerchant = (id) => axios.delete(`${urlMerchant}/${id}`);
 
-const urlCustomer = "http://localhost:5000/customer";
+const urlCustomer = "https://multi-server1.herokuapp.com/customer";
 export const fetchCustomer = () => axios.get(urlCustomer);
 export const createCustomer = (newCustomer) =>
   axios.post(urlCustomer, newCustomer);
